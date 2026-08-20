b54d7edd9dee2b871ef3b3989f0b3c84494cacd6
Thomas Wagner <mrthomaswagner@gmail.com>
2026-08-19 23:22:44 -0400

    thru chap 12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,56 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+use minigrep::search;
+use minigrep::search_case_insensitive;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &file_contents)
+    } else {
+        search(&config.query, &file_contents)
+    };
+
+    for line in results {
+        println!("{line}"); 
+    }
+    
+    Ok(())
+}
+
+struct Config {
+    file_path: String,
+    query: String,
+    ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        Ok(Config {
+            file_path: args[2].clone(),
+            query: args[1].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
+        })
+    }
 }
